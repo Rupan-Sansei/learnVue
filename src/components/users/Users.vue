e0ba1c44378bd2c54dd7ced0a905e4ffec65d039
Rupan-Sansei <1078551406@qq.com>
2021-03-20 21:26:48 +0800

    更新

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -2,9 +2,7 @@
   <div>
     <!-- 面包屑导航 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/home' }">
-        首页
-      </el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/home' }"> 首页 </el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
@@ -18,10 +16,7 @@
             placeholder="请输入内容"
             clearable
           >
-            <el-button
-              slot="append"
-              icon="el-icon-search"
-            />
+            <el-button slot="append" icon="el-icon-search" />
           </el-input>
         </el-col>
         <el-col :span="4">
@@ -31,62 +26,28 @@
         </el-col>
       </el-row>
       <!-- 表格 -->
-      <el-table
-        :data="userlist"
-        stripe
-        border
-      >
+      <el-table :data="userlist" stripe border>
         <el-table-column type="index" />
-        <el-table-column
-          label="姓名"
-          prop="usename"
-        />
-        <el-table-column
-          label="邮箱"
-          prop="email"
-        />
-        <el-table-column
-          label="电话"
-          prop="mobile"
-        />
-        <el-table-column
-          label="角色"
-          prop="role_name"
-        />
-        <el-table-column
-          label="状态"
-          prop="mg_state"
-        >
+        <el-table-column label="姓名" prop="usename" />
+        <el-table-column label="邮箱" prop="email" />
+        <el-table-column label="电话" prop="mobile" />
+        <el-table-column label="角色" prop="role_name" />
+        <el-table-column label="状态" prop="mg_state">
           <template slot-scope="scope">
             <el-switch v-model="scope.row.mg_state" />
           </template>
         </el-table-column>
-        <el-table-column
-          label="操作"
-          width="180px"
-        >
+        <el-table-column label="操作" width="180px">
           <template>
-            <el-button
-              type="primary"
-              icon="el-icon-edit"
-              size="mini"
-            />
-            <el-button
-              type="danger"
-              icon="el-icon-delete"
-              size="mini"
-            />
+            <el-button type="primary" icon="el-icon-edit" size="mini" />
+            <el-button type="danger" icon="el-icon-delete" size="mini" />
             <el-tooltip
               effect="dark"
               content="分配权限"
               placement="top"
               :enterable="false"
             >
-              <el-button
-                type="warning"
-                icon="el-icon-setting"
-                size="mini"
-              />
+              <el-button type="warning" icon="el-icon-setting" size="mini" />
             </el-tooltip>
           </template>
         </el-table-column>
@@ -101,12 +62,13 @@
         @current-change="handleCurrentChange"
       />
     </el-card>
-    <el-dialog
-      title="添加用户"
-      :visible.sync="dialogVisible"
-      width="50%"
+    <el-dialog title="添加用户" :visible.sync="dialogVisible" width="50%">
+      <el-form
+        :model="addform"
+        :rules="addrules"
+        label-width="100px"
+        class="demo-ruleForm"
       >
-      <el-form :model="addform" :rules="addrules"  label-width="100px" class="demo-ruleForm" >
         <el-form-item label="用户名称" prop="usename">
           <el-input v-model="addform.usename"></el-input>
         </el-form-item>
@@ -122,7 +84,9 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确 定</el-button
+        >
       </span>
     </el-dialog>
   </div>
@@ -130,87 +94,90 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
+      list: [],
       userlist: [
         {
-          usename: '曹尼玛',
-          email: '1012451231@qq.com',
-          mobile: '2132132135',
-          role_name: '大财主',
-          mg_state: true
+          usename: "曹尼玛",
+          email: "1012451231@qq.com",
+          mobile: "2132132135",
+          role_name: "大财主",
+          mg_state: true,
         },
         {
-          usename: '尼玛曹',
-          email: '1mnhgf@qq.com',
-          mobile: '5586435',
-          role_name: '小财主',
-          mg_state: false
+          usename: "尼玛曹",
+          email: "1mnhgf@qq.com",
+          mobile: "5586435",
+          role_name: "小财主",
+          mg_state: false,
         },
         {
-          usename: '玛曹尼',
-          email: 'vxcvxcvxcv31@qq.com',
-          mobile: '2369665',
-          role_name: '农夫',
-          mg_state: true
+          usename: "玛曹尼",
+          email: "vxcvxcvxcv31@qq.com",
+          mobile: "2369665",
+          role_name: "农夫",
+          mg_state: true,
         },
         {
-          usename: '康尼玛',
-          email: '2315asdasd31@qq.com',
-          mobile: '97415565',
-          role_name: '小偷',
-          mg_state: true
-        }, {
-          usename: '康尼玛',
-          email: 'xcvfca@qq.com',
-          mobile: '110',
-          role_name: '大财主',
-          mg_state: true
-        }
+          usename: "康尼玛",
+          email: "2315asdasd31@qq.com",
+          mobile: "97415565",
+          role_name: "小偷",
+          mg_state: true,
+        },
+        {
+          usename: "康尼玛",
+          email: "xcvfca@qq.com",
+          mobile: "110",
+          role_name: "大财主",
+          mg_state: true,
+        },
       ],
       queryinfo: {
-        query: '',
+        query: "",
         // 当前页数
         pagenum: 1,
         // 每页显示数
-        pagesize: 2
+        pagesize: 2,
       },
       total: 5,
-      dialogVisible:false,
-      addform:{
-        usename:'',
-        password:'',
-        email:'',
-        phone:''
+      dialogVisible: false,
+      addform: {
+        usename: "",
+        password: "",
+        email: "",
+        phone: "",
       },
-      addrules:{
-        usename:[
-          { required: true, message: '请输入用户名', trigger: 'blur' },
-          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+      addrules: {
+        usename: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
         ],
-        password:[
-          { required: true, message: '请输入密码', trigger: 'blur' },
-        ],
-        email:[
-          { required: true, message: '请输入邮箱', trigger: 'blur' },
-        ],
-        phone:[
-          { required: true, message: '请输入电话', trigger: 'blur' },
-        ]
-      }
-    }
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+        email: [{ required: true, message: "请输入邮箱", trigger: "blur" }],
+        phone: [{ required: true, message: "请输入电话", trigger: "blur" }],
+      },
+    };
   },
   methods: {
     // 监听pagesize变化
-    handleSizeChange (newsize) {
-      console.log(newsize)
+    handleSizeChange(newsize) {
+      console.log(newsize);
     },
     // 监听页码值
-    handleCurrentChange (newpage) {
-      console.log(newpage)
-    }
-  }
-}
+    handleCurrentChange(newpage) {
+      console.log(newpage);
+    },
+    async getlist() {
+      const a = await this.$http.get("http://localhost:3000/api/users/list");
+      this.list = a.data.data.lists;
+    },
+  },
+  created() {
+    this.getlist();
+  },
+};
 </script>
 
 <style lang="less" scoped>
